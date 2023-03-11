e86563f1fdff7385c80242b05af950a86609dd15
Svetozar Stanev <svetozar.stanev@gmail.com>
2023-03-11 17:37:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Typography,
+  Box,
+  OutlinedInput,
+  InputAdornment,
+  InputLabel,
+  FormControl,
+  styled,
+} from "@mui/material";
+
+const PageContainer = styled(Box)(() => ({
+  textAlign: "center",
+  padding: "50px",
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <Typography variant="h3">Finance Tracker</Typography>
+      <Typography
+        variant="h5"
+        sx={{
+          margin: "30px 0",
+        }}
+      >
+        Balance: 4567,00 BGN
+      </Typography>
+      <FormControl fullWidth sx={{ m: 1 }}>
+        <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-amount"
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+          label="Amount"
+        />
+      </FormControl>
+    </PageContainer>
   );
 }
 
